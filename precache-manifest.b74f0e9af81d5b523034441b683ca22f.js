d7a8685255ac34903b1e1a3c875037068dd7ca83
LeeTaeWoong <bugkingus@gmail.com>
2019-11-28 03:23:54 +0900

    Updates

---
+++ precache-manifest.b74f0e9af81d5b523034441b683ca22f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db1d2eb8becbdb14db5544d7234b3bd1",
+    "url": "/react_app_2019/index.html"
+  },
+  {
+    "revision": "14f9ac11abefd618645b",
+    "url": "/react_app_2019/static/css/main.571632d6.chunk.css"
+  },
+  {
+    "revision": "a8f58841cb8d5fb2bd8c",
+    "url": "/react_app_2019/static/js/2.06ee0613.chunk.js"
+  },
+  {
+    "revision": "14f9ac11abefd618645b",
+    "url": "/react_app_2019/static/js/main.eef6f028.chunk.js"
+  },
+  {
+    "revision": "5b6416dbe6d0ece82e20",
+    "url": "/react_app_2019/static/js/runtime-main.f388e569.js"
+  }
+]);
